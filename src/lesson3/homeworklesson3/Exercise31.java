a70f9ff0ee498db30d4626be473b4ce01373c557
coval <covalenco.alexandr@gmail.com>
2023-11-12 19:57:03 +0100

    Initial commit

---
+++ src/lesson3/homeworklesson3/Exercise31.java
@@ -0,0 +1,9 @@
+package lesson3.homeworklesson3;
+
+public class Exercise31 {
+    public static void main(String[] args) {
+        System.out.println("Меня зовут Александр.");
+        System.out.println("Мне 42 года.");
+        System.out.println("Я из Молдовы.");
+    }
+}
